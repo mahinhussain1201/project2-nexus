e2da045f26485f53db722ab1225b7c15a0ae99c1
mahinhussain1201 <mahinhussain1201@gmail.com>
2024-01-31 20:42:13 +0530

    new

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2lkzy^atuhai&u%)vm7=xaq%*=ql%$+rk_=x$kie=lnl-+9ifo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1' ]
+ALLOWED_HOSTS = ['.vercel.app', '.netlify.app','127.0.0.1' ]
 
 
 # Application definition
